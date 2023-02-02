98ac911e88d2e80b4ab85926715279ebef0275e5
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2023-02-01 19:05:42 -0600

    turbo drive examples

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def page1
+  end
+
+  def page2
+    sleep 3
+  end
+
+  def page3
+  end
+
+  def confirmation
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'page1', to:'pages#page1'
+  get 'page2', to:'pages#page2'
+  get 'page3', to:'pages#page3'
+  get 'confirmation', to:'pages#confirmation'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
