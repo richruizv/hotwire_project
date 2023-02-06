06def382753fd120078104540bdad246f96fae9c
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2023-02-06 14:30:19 -0600

    refactore  render turbo steam code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def render_turbo_stream_flash_messages
+    turbo_stream.prepend "flash", partial: "layouts/flash"
+  end
+  
   def show_svg(path)
     File.open("app/assets/images/#{path}", "rb") do |file|
       raw file.read
