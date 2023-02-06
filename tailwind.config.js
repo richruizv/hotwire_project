81e51b140d6bfebbfa92cfa7e8c3ec77880845c7
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2023-02-06 14:24:46 -0600

    add flash message with custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,18 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'appear-then-fade': 'appear-then-fade 4s both',
+      },
+      keyframes: {
+        'appear-then-fade': {
+          '0%, 100%': { opacity: '0' },
+          '5%, 60%': { opacity: '1' }
+        }
+      }
+    }
+  }
 }
